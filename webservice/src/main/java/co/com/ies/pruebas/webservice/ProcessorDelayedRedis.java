7a85c424afc18975a4b0189d1228f6e67a0f5c1d
andres.montoya <andres.montoya@iesonline.com.co>
2021-08-19 16:51:26 -0500

    feat: configuracion y funciones iniciales

---
+++ webservice/src/main/java/co/com/ies/pruebas/webservice/ProcessorDelayedRedis.java
@@ -0,0 +1,76 @@
+package co.com.ies.pruebas.webservice;
+
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Calendar;
+import java.util.Optional;
+
+@Component
+public class ProcessorDelayedRedis {
+
+    private final GreetingRepository greetingRepository;
+
+    private final String hostAddress;
+
+    private final FinishedTasckRedis finishedTasck;
+
+    public ProcessorDelayedRedis(FinishedTasckRedis finishedTasck, GreetingRepository greetingRepository) {
+        this.finishedTasck = finishedTasck;
+        String hostAddress1;
+        this.greetingRepository = greetingRepository;
+        try {
+            hostAddress1 = InetAddress.getLocalHost().getHostAddress() ;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            hostAddress1 = "no found";
+        }
+        this.hostAddress = hostAddress1;
+    }
+
+
+    public void processElement(Greeting greeting) {
+        //final int timeSleep = rn.nextInt(2000);
+        final int timeSleep = 2000;
+
+        try {
+            Thread.sleep(timeSleep);
+            System.out.println("ProcessorDelayedRedis.processElement resolviendo element = " + greeting + " ,timeSleep = "+ timeSleep);
+
+
+//TODO verificar que ya ha sido tramitado para ver la tasa de falsos positivos
+
+            verifyExist(greeting);
+
+            greeting.setIpTramited(hostAddress);
+            final long timeInMillis = Calendar.getInstance().getTimeInMillis();
+            greeting.setTime(timeInMillis);
+
+            greetingRepository.save(greeting);
+            greetingRepository.flush();
+            finishedTasck.offer(greeting);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void verifyExist(Greeting task) {
+        try {
+            final Optional<Greeting> optional = greetingRepository.findById(task.getId());
+            if (optional.isPresent()) {
+
+                final Greeting greeting = optional.get();
+                if (greeting.getIpTramited() != null) {
+                    System.out.println("ProcessorDelayedRedis.processElement byId = " + greeting + "<<<<<<<<<<<<<<<<<<<<<<<<<<");
+                }
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
---
+++ webservice/src/main/java/co/com/ies/pruebas/webservice/QueueAsyncAbstract.java
@@ -0,0 +1,81 @@
+package co.com.ies.pruebas.webservice;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
+
+public abstract class QueueAsyncAbstract<TaskType> {
+
+    protected abstract void offer(TaskType element);
+
+    protected abstract void updateElement(TaskType element);
+    protected abstract boolean isContains(TaskType element, Queue<TaskType> queue);
+    protected abstract boolean isContains(TaskType element);
+    protected abstract boolean remove(TaskType element);
+    protected abstract Queue<TaskType> getQueue();
+    protected abstract void processElement(TaskType element);
+    protected abstract int size();
+
+    /**
+     *
+     * @param elements
+     * @deprecated no es efectivo para el metodo que verifica si ya esta adentro
+     */
+    @Deprecated
+    public void offerTascks(List<TaskType> elements){
+        //agregar a la cola
+        elements.forEach(this::offer);
+
+    }
+
+    public void offerTasck(TaskType element){
+        //agregar a la cola
+        offer(element);
+
+    }
+
+    public void processQueue(){
+        Queue<TaskType> elements = getQueue();
+        System.out.println("QueueAsyncAbstract.processQueue elements = " + elements.size());
+        final Iterator<TaskType> iterator = elements.iterator();
+
+        while (iterator.hasNext()){
+            final TaskType tasckType = iterator.next();
+            try {
+                processElement(tasckType);
+                elements.remove(tasckType);
+                iterator.remove();
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Error al procesar la tarea, se agrega de nuevo en la cola");
+                offer(tasckType);
+            }
+        }
+
+
+    }
+
+    private void packProcess(int limit) {
+        Queue<TaskType> elementsPack = getQueue();
+        System.out.println("QueueAsyncAbstract.processQueue elementsPack = " + elementsPack.size());
+        // use un iterator en eves del for y un poll por que me quedaban completas las tareas
+        final Iterator<TaskType> iterator = elementsPack.iterator();
+        int count = 0;
+        while (iterator.hasNext() && count < limit){
+            final TaskType tasckType = iterator.next();
+            try {
+                processElement(tasckType);
+                elementsPack.remove(tasckType);
+                iterator.remove();
+                count++;
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Error al procesar la tarea, se agrega de nuevo en la cola");
+                offer(tasckType);
+            }
+        }
+    }
+
+
+}
+
